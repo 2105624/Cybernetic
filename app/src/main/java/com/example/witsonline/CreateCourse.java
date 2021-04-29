2fde3f2bb34cd646a053f4a572565b5122e7b161
2105624 <2105624@students.wits.ac.za>
2021-04-29 12:09:20 +0200

    cleared old course page to add in actual create course activity

--- app/src/main/java/com/example/witsonline/CreateCourse.java
+++ app/src/main/java/com/example/witsonline/CreateCourse.java
@@ -26,138 +26,18 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
-public class CreateCourse extends AppCompatActivity implements View.OnScrollChangeListener, BottomNavigationView.OnNavigationItemSelectedListener  {
-    //Creating a list of Courses
-    private ArrayList<CourseV> listCourseVs;
+public class CreateCourse extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
 
-    //CreatingViews
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
-    private RecyclerView.Adapter adapter;
-    String webURL = "https://lamp.ms.wits.ac.za/home/s2105624/loadCourses.php?page=";
-
-    //Volley Request Queue
-    private RequestQueue requestQueue;
-
-    //The request counter to send ?page=1, ?page=2 requests
-    private int courseCount = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_course);
-
-        //Initializing Views
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        //Initializing our Course list
-        listCourseVs = new ArrayList<>();
-        requestQueue = Volley.newRequestQueue(this);
-
-        //Calling method getData to fetch data
-        getData();
-
-        //Adding an scroll change listener to recyclerView
-        recyclerView.setOnScrollChangeListener(this);
-
-        //initializing our adapter
-        adapter = new CourseCardAdapter(listCourseVs, this);
-
-        //Adding adapter to recyclerview
-        recyclerView.setAdapter(adapter);
-
-        /*
-        USER.COURSES = new ArrayList<String>();
-
-        USER.COURSES.add("COMS1018A");
-        USER.COURSES.add("COMS3007A");
-        USER.COURSES.add("DUMMY1000");
-        */
 
         BottomNavigationView dashboardBottomNavigation = findViewById(R.id.dashboardBottomNavigation);
         dashboardBottomNavigation.setOnNavigationItemSelectedListener(CreateCourse.this);
 
         dashboardBottomNavigation.inflateMenu(R.menu.menu_instructor);
         dashboardBottomNavigation.setSelectedItemId(R.id.menuCreateCourse);
-    }
-
-    //Request to get json from server we are passing an integer here
-    //This integer will used to specify the page number for the request ?page = requestcount
-    //This method would return a JsonArrayRequest that will be added to the request queue
-    private JsonArrayRequest getDataFromServer(int requestCount){
-        //Initializing progressbar
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar1);
-
-        //Displaying ProgressBar
-        progressBar.setVisibility(View.VISIBLE);
-        setProgressBarIndeterminateVisibility(true);
-
-        //JsonArrayRequest of volley
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(webURL + String.valueOf(requestCount),
-                (response) -> {
-                    //Calling method parseData to parse the json responce
-                    try {
-                        parseData(response);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    //Hiding the progressBar
-                    progressBar.setVisibility(View.GONE);
-                },
-                (error) -> {
-                    progressBar.setVisibility(View.GONE);
-                    //If an error occurs that means end of the list has been reached
-                    Toast.makeText(CreateCourse.this, "No More Items Available", Toast.LENGTH_SHORT).show();
-                });
-        //Returning the request
-        return jsonArrayRequest;
-    }
-
-    //This method will get Data from the web api
-    private void getData(){
-        //Adding the method to the queue by calling the method getDatafromServer
-        requestQueue.add(getDataFromServer(courseCount));
-        //Incrementing the course counter
-        courseCount++;
-    }
-
-    //This method will parse json Data
-    private void parseData(JSONArray array) throws JSONException {
-        for (int i = 0; i< array.length(); i++) {
-            // Creating the Course object
-            CourseV courseV = new CourseV();
-            JSONObject json = null;
-            try {
-                //Getting json
-                json = array.getJSONObject(i);
-
-                //Adding data to the course object
-                courseV.setCourseName(json.getString("courseName"));
-                courseV.setCourseDescription(json.getString("courseDescription"));
-                courseV.setCourseInstructor(json.getString("courseInstructor"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            //Adding the request object to the list
-            if (json.getString("courseInstructor").equals("richard.klein")) {
-                listCourseVs.add(courseV);
-                adapter.notifyDataSetChanged();
-            }
-
-            /*
-            if ((USER.COURSES != null) && (USER.COURSES.contains(json.getString("courseCode")))) {
-                listCourseVs.add(courseV);
-                adapter.notifyDataSetChanged();
-            }
-            */
-
-            //listCourseVs.add(courseV);
-
-            //Notifying the adapter that data has been added or changed
-            //adapter.notifyDataSetChanged();
-        }
     }
 
     //This method will check if the recyclerview has reached the bottom or not
@@ -171,14 +51,6 @@ public class CreateCourse extends AppCompatActivity implements View.OnScrollChan
         return false;
     }
 
-    @Override
-    public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-        //if Scrolled at last then
-        if(isLastItemDistplaying(recyclerView)){
-            //Calling the method getData again
-            getData();
-        }
-    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {

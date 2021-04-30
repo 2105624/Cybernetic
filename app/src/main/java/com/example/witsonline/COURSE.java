0efed4d3809c70e6d7d033adbe0f05767ebfee96
2105624 <2105624@students.wits.ac.za>
2021-04-30 09:04:48 +0200

    testing push

--- app/src/main/java/com/example/witsonline/COURSE.java
+++ app/src/main/java/com/example/witsonline/COURSE.java
@@ -3,6 +3,7 @@ package com.example.witsonline;
 import java.util.ArrayList;
 
 public class COURSE {
+
         public static String CODE;
         public static String NAME;
 	public static String DESCRIPTION;

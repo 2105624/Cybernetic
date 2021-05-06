317ba318be1338295c841ee567a18179f1c2b3a4
Emanuel <2139555@students.wits.ac.za>
2021-05-06 16:43:10 +0200

    Reviews displaying in instructor course home

--- app/src/main/java/com/example/witsonline/CourseHomePageInstructor.java
+++ app/src/main/java/com/example/witsonline/CourseHomePageInstructor.java
@@ -1,40 +1,64 @@
 package com.example.witsonline;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class CourseHomePageInstructor extends AppCompatActivity implements View.OnScrollChangeListener {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+public class CourseHomePageInstructor extends AppCompatActivity implements  View.OnScrollChangeListener{
     LinearLayout outlineLayout;
-    Button subscribe;
-    Button review;
+    boolean browse;
+    //Creating a list of Courses
     private ArrayList<ReviewV> listReviewVs;
     private ImageView image;
+    //This is for the unsubscribe pop up menu
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private Button btnUnsubscribe, btnCancel;
+
+    //This is for the review pop up menu
+    private EditText edtReviewDescription;
+    private RatingBar rtbReviewRating;
+    private Button btnReviewPostReview;
 
     //CreatingViews
     private RecyclerView recyclerView;
@@ -47,20 +71,25 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
 
     //The request counter to send ?page=1, ?page=2 requests
     private int reviewCount = 1;
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_home_page_instructor);
+        super.onCreate(savedInstanceState);
         TextView courseName =(TextView)findViewById(R.id.courseName);
         TextView courseDescription =(TextView)findViewById(R.id.courseDescription);
         TextView courseInstructor =(TextView)findViewById(R.id.courseInstructor);
         RatingBar courseRating = (RatingBar)findViewById(R.id.courseRating);
-        subscribe = (Button)findViewById(R.id.subscribe);
         outlineLayout = findViewById(R.id.courseOutline);
-        review = (Button)findViewById(R.id.review);
         image = (ImageView)findViewById(R.id.courseImage);
 
+        //To determine which activity we came from (BrowseCourses or MyCourses
+        //  Toast.makeText(CourseHomePage.this, act, Toast.LENGTH_LONG).show();
 
+        /*
+        if(!COURSE.IMAGE.equals("null")){
+            Glide.with(this).load(COURSE.IMAGE).into(image);
+        }*/
         courseName.setText(COURSE.NAME);
         courseDescription.setText(COURSE.DESCRIPTION);
         courseInstructor.setText("By: "+COURSE.INSTRUCTOR);
@@ -85,11 +114,12 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
 
         //initializing our adapter
         adapter = new ReviewCardAdapter(listReviewVs, this);
+
+        //Adding adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
     }
-    //Request to get json from server we are passing an integer here
-    //This integer will used to specify the page number for the request ?page = requestCount
-    //This method would return a JsonArrayRequest that will be added to the request queue
-    private JsonArrayRequest getDataFromServer(int requestCount) {
+    private JsonArrayRequest getDataFromServer(int requestCount){
         //Initializing progressbar
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.reviewProgressBar);
 
@@ -113,18 +143,16 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
         //Returning the request
         return jsonArrayRequest;
     }
-
     //This method will get Data from the web api
-    private void getData() {
+    private void getData(){
         //Adding the method to the queue by calling the method getDatafromServer
         requestQueue.add(getDataFromServer(reviewCount));
         //Incrementing the course counter
         reviewCount++;
     }
-
     //This method will parse json Data
-    private void parseData(JSONArray array) {
-        for (int i = 0; i < array.length(); i++) {
+    private void parseData(JSONArray array){
+        for (int i = 0; i< array.length(); i++){
             // Creating the Course object
             ReviewV reviewV = new ReviewV();
             JSONObject json = null;
@@ -137,7 +165,7 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
                 reviewV.setStudentLName(json.getString("reviewStudentLName"));
                 reviewV.setReviewRating(json.getString("reviewRating"));
                 reviewV.setReviewDescription(json.getString("reviewDescription"));
-            } catch (JSONException e) {
+            } catch (JSONException e){
                 e.printStackTrace();
             }
             //Adding the request object to the list
@@ -146,12 +174,11 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
         //Notifying the adapter that data has been added or changed
         adapter.notifyDataSetChanged();
     }
-
     //This method will check if the recyclerview has reached the bottom or not
-    private boolean isLastItemDisplaying(RecyclerView recyclerView) {
-        if (recyclerView.getAdapter().getItemCount() != 0) {
+    private boolean isLastItemDisplaying(RecyclerView recyclerView){
+        if(recyclerView.getAdapter().getItemCount() != 0){
             int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
-            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1) {
+            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() -1){
                 return true;
             }
         }
@@ -164,25 +191,37 @@ public class CourseHomePageInstructor extends AppCompatActivity implements View.
         outlineLayout.removeAllViews();
 
         //for each topic create a card to come up:
-        for (int i = 0; i < outlineTopics.length; i++) {
-            View topicView = View.inflate(this, R.layout.outline_topic_entry, null);
+        for(int i=0; i < outlineTopics.length; i++)
+        {
+            View topicView = View.inflate(this, R.layout.outline_topic_entry ,null);
             TextView topicName = topicView.findViewById(R.id.outlineTopic);
 
-            topicName.setText("➤ " + outlineTopics[i]);
+            topicName.setText("➤ "+ outlineTopics[i]);
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            params.setMargins(10, 0, 10, 0);
+            params.setMargins(10,0,10,0);
 
             outlineLayout.addView(topicView, params);
         }
     }
-
     @Override
     public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
         //if Scrolled at last then
-       if (isLastItemDisplaying(recyclerView)) {
+        if(isLastItemDisplaying(recyclerView)){
             //Calling the method getData again
             getData();
         }
     }
+   /*    @Override
+ public void onBackPressed(){
+        if (browse){
+            Intent intent = new Intent(CourseHomePageInstructor.this, BrowseCourses.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Intent intent = new Intent(CourseHomePageInstructor.this, MyCourses.class);
+            startActivity(intent);
+            finish();
+        }
+    }*/
 }

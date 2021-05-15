e6a922e13198664f2c1956dbe9564ca63d073bb1
2105624 <2105624@students.wits.ac.za>
2021-05-15 22:04:21 +0200

    can upload pdfs to server for a lesson

--- app/src/main/java/com/example/witsonline/CreateLesson.java
+++ app/src/main/java/com/example/witsonline/CreateLesson.java
@@ -1,16 +1,25 @@
 package com.example.witsonline;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
@@ -23,6 +32,8 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,6 +49,14 @@ public class CreateLesson extends AppCompatActivity {
     private TextInputLayout lessonText;
     private Button btnCreate;
 
+    //for adding a pdf
+    private TextView selectedFile;
+    private ImageButton btnUploadFile;
+    private int REQ_PDF = 21;
+    private String encodedPDF;
+    public static final int STORAGE_PERMISSION_CODE = 123;
+    private boolean fileSelected = false;
+
 
     //Insertion
     private RequestQueue requestQueue;
@@ -47,10 +66,26 @@ public class CreateLesson extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_lesson);
 
+        //reuqest storage permission
+        requestStoragePermission();
+
         //finding views
         lessonName = findViewById(R.id.lessonName);
         lessonText = findViewById(R.id.lessonText);
         btnCreate = findViewById(R.id.buttonCreateLesson);
+        selectedFile = findViewById(R.id.selectedFileText);
+        btnUploadFile = findViewById(R.id.imageButtonUploadFile);
+
+        //selecting a file
+        btnUploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseFile.setType("application/pdf");
+                chooseFile = Intent.createChooser(chooseFile,"Choose a file");
+                startActivityForResult(chooseFile,REQ_PDF);
+            }
+        });
 
         //the insertion
         requestQueue = Volley.newRequestQueue(getApplicationContext());
@@ -60,6 +95,9 @@ public class CreateLesson extends AppCompatActivity {
                 if (isEmpty(lessonName) | isEmpty(lessonText)){
                     //error messages will be displayed
                 }else{
+                    if (!fileSelected){
+                        encodedPDF = "nofile";
+                    }
                     StringRequest request = new StringRequest(Request.Method.POST, insertURL, new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
@@ -77,6 +115,7 @@ public class CreateLesson extends AppCompatActivity {
                             parameters.put("name", lessonName.getEditText().getText().toString().trim());
                             parameters.put("course", COURSE.CODE);
                             parameters.put("text", lessonText.getEditText().getText().toString().trim());
+                            parameters.put(("fs"),encodedPDF);
                             return parameters;
                         }
                     };
@@ -95,7 +134,7 @@ public class CreateLesson extends AppCompatActivity {
       //  mediaController = new MediaController(this);
       //  mediaController.setAnchorView(videoView);
        // videoView.setMediaController(mediaController);
-       Uri uri = Uri.parse("http://web.logiclabsolutions.com/xxx.mp4");
+    //   Uri uri = Uri.parse("http://web.logiclabsolutions.com/xxx.mp4");
        // Uri uri = Uri.parse("https://www.youtube.com/watch?v=teiIw6Zp6X8");
       //  videoView.setVideoURI(uri);
       //  videoView.requestFocus();
@@ -103,6 +142,60 @@ public class CreateLesson extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_PDF && resultCode == RESULT_OK && data != null){
+            Uri path = data.getData();
+            if (path != null) {
+                try {
+                    fileSelected = true;
+                    InputStream inputStream = CreateLesson.this.getContentResolver().openInputStream(path);
+                    byte[] pdfInBytes = new byte[inputStream.available()];
+                    inputStream.read(pdfInBytes);
+                    encodedPDF = Base64.encodeToString(pdfInBytes, Base64.DEFAULT);
+
+                    //Toast.makeText(CreateLesson.this, "Document selected",Toast.LENGTH_SHORT).show();
+                    selectedFile.setText("File selected");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                fileSelected = false;
+            }
+        }
+    }
+
+    private void requestStoragePermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            //can explain here why you need this permission.
+        }
+        //ask for permission
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+    }
+
+    //This method will be called when user taps on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+
+        //Checking if request code is our request
+        if (requestCode == STORAGE_PERMISSION_CODE){
+
+            //if granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission granted",Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "Permission denied",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     // This function checks if a required text is empty or not
